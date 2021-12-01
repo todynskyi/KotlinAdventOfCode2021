1fe458d3bb35f6a981ab8988d532fc6fc000a5f5
todynskyi <todynskyi@gmail.com>
2021-12-02 00:28:34 +0200

    Solved Day1 assigment

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(data: List<Long>): Int =
+        List(data.size) { index -> if (index == 0 || data[index - 1] >= data[index]) 0 else 1 }.sum()
+
+    fun part2(data: List<Long>): Int {
+        val sumDepths = data.mapIndexed { index, value ->
+            value + data.getOrElse(index + 1) { 0 } + data.getOrElse(index + 2) { 0 }
+        }
+        return part1(sumDepths)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toLong() }
     println(part1(input))
     println(part2(input))
 }
